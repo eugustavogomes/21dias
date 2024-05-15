a264385fb1c4a04c0b7949bdb5dbfc72d43aac3c
Gustavo Gomes <lgglima@gmail.com>
2024-05-14 23:11:33 -0400

    atualizando 2/3 dia

--- 2dia/index.js
+++ 2dia/index.js
@@ -1,1 +1,17 @@
-console.log('olá')
+let nome = ""
+let idade = 0
+let altura = 0
+let peso = 0
+
+nome = prompt("Insira seu nome")
+idade = parseInt(prompt("Insira sua idade"))
+altura = parseFloat(prompt("Insira sua altura"))
+peso = parseInt(prompt("Insira seu peso"))
+
+let anoDeNascimento = 0
+anoDeNascimento = 2024 - idade
+
+let imc = 0
+imc = peso/(altura*altura)
+
+console.log("Olá, " + nome +  ", sua idade é " + idade + " anos, seu ano de nascimento é " + anoDeNascimento + ", e seu IMC é " + imc )
